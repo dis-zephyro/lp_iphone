74e0a90c1b417860e825a62e767583edb859e9b1
dis-zephyro <zephyro@yandex.ru>
2016-01-20 05:45:22 +0300

    v1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.reviews').slick({
+    dots: true,
+    arrows: true,
+    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<span class="reviews-nav prev">',
+    nextArrow: '<span class="reviews-nav next">'
+
+});
